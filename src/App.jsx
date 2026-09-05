1a025d53ca912265db54f37c8e467cd9d079f722
Aarti Sushmesh <134151553+Aartisushmesh29@users.noreply.github.com>
2026-09-05 22:27:14 +0530

    Refactor App component for improved structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Header from "./pages/Header/Header";
 import Hero from "./pages/Hero/Hero";
 import Skills from "./pages/Skills/Skills";
 import Education from "./pages/Education/Education";
-import { Analytics } from "@vercel/analytics/next"
+
 
 import { Route, Routes } from "react-router-dom";
 
@@ -17,7 +17,6 @@ export default function App() {
   return (
     <>
       <Header />
-      <Analytics/>
       {/* Conditional Rendering */}
       {isOnePage ? (
         // One-Page Mode: Render all components together
